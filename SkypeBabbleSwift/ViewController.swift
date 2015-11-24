9244cddc7d6b7af82e98de3f640c75be21147dac
James O'Brien <jamesobrien@jamess-macbook-pro.local>
2015-11-24 16:18:28 +0000

    Initial Commit

---
+++ SkypeBabbleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkypeBabbleSwift
+//
+//  Created by James O'Brien on 24/11/2015.
+//  Copyright © 2015 James O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
